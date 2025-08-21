32914343cf5c222ce67009c20c58babe029f1df9
moronnahuu1 <nahuelarielmoron1@gmail.com>
2025-08-21 13:46:00 -0300

    environments and budgets for build

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:3000/', ///LOCAL;
+  //endpoint: 'http://localhost:3000/', ///LOCAL;
+  endpoint: 'https://api.safe-365.online/', ///PROD;
 };

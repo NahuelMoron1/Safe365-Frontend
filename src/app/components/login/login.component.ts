f27f71d614db73450be64580f0ecac05d77c58b4
moronnahuu1 <nahuelarielmoron1@gmail.com>
2025-08-27 12:43:27 -0300

    final steps of linting, everything working as expected, finished removing some unused variables and imports, think everything is right way linted now

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { SkyToastService } from '@skyux/toast';
 import { UserRole } from '../../models/enums/UserRole';
 import { User } from '../../models/User';
 import { AuthRedirectService } from '../../services/auth-redirect.service';
@@ -17,12 +15,10 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 })
 export class LoginComponent {
   private userService = inject(UserService);
-  private toastSvc = inject(SkyToastService);
   private errorService = inject(ErrorService);
   private redirectService = inject(AuthRedirectService);
 
   public user: User = new User('', '', '', '', '', UserRole.CLIENT);
-  private route?: ActivatedRoute;
 
   async login() {
     const emailInp = document.getElementById('emailInp') as HTMLInputElement;
@@ -31,19 +27,21 @@ export class LoginComponent {
     ) as HTMLInputElement;
     let email = '';
     let password = '';
-    if (emailInp && passwordInp) {
-      email = emailInp.value;
-      password = passwordInp.value;
-      try {
-        const access = await this.userService
-          .login(email, password)
-          .toPromise();
-        if (access) {
-          window.location.href = this.redirectService.getRedirectUrl() || '/';
-        }
-      } catch (error: any) {
-        this.errorService.handleError(error, 'No se pudo iniciar sesión');
+    if (!emailInp || !passwordInp) {
+      return this.errorService.handleError(
+        undefined,
+        'No todos los campos contienen un valor'
+      );
+    }
+    email = emailInp.value;
+    password = passwordInp.value;
+    try {
+      const access = await this.userService.login(email, password).toPromise();
+      if (access) {
+        window.location.href = this.redirectService.getRedirectUrl() || '/';
       }
+    } catch (error: any) {
+      this.errorService.handleError(error, 'No se pudo iniciar sesión');
     }
   }
 }
